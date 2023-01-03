d467ffbb30ad759316df76e72ea06b413bc29a31
Petr Plsek <me@kalanys.com>
2023-01-03 02:19:34 +0100

    kw_files - support processor extended

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ in classical filesystems.
 ```
 {
     "require": {
-        "alex-kalanis/kw_files": "2.0"
+        "alex-kalanis/kw_files": "2.1"
     }
 }
 ```
--- php-tests/ExtendedTests/ProcessorTest.php
+++ php-tests/ExtendedTests/ProcessorTest.php
@@ -19,24 +19,27 @@ class ProcessorTest extends CommonTestClass
      */
     public function testProcess(): void
     {
-        $memory = new Memory();
-        $storage = new \kalanis\kw_storage\Storage\Storage(new DefaultKey(), $memory);
-        $dir = new Storage\ProcessDir($storage);
-        $node = new Storage\ProcessNode($storage);
-        $lib = new Processor($dir, $node, new Config());
+        $storage = new \kalanis\kw_storage\Storage\Storage(new DefaultKey(), new Memory());
+        $lib = new Processor(new Storage\ProcessDir($storage), new Storage\ProcessNode($storage), new Config());
 
-        $this->assertFalse($node->exists(['foo', 'bar']));
+        $this->assertFalse($lib->exists(['foo', 'bar']));
+        $this->assertFalse($lib->dirExists(['foo', 'bar']));
+        $this->assertFalse($lib->fileExists(['foo', 'bar']));
         $this->assertTrue($lib->createDir(['foo', 'bar']));
-        $this->assertTrue($node->exists(['foo', 'bar']));
-        $this->assertFalse($node->exists(['foo', 'bar', '.txt']));
+        $this->assertTrue($lib->exists(['foo', 'bar']));
+        $this->assertFalse($lib->fileExists(['foo', 'bar']));
+        $this->assertTrue($lib->dirExists(['foo', 'bar']));
+        $this->assertFalse($lib->exists(['foo', 'bar', '.txt']));
         $this->assertFalse($lib->createDir(['foo', 'bar'], true));
         $this->assertTrue($lib->makeExtended(['foo', 'bar']));
-        $this->assertTrue($node->exists(['foo', 'bar', '.txt']));
+        $this->assertTrue($lib->exists(['foo', 'bar', '.txt']));
+        $this->assertTrue($lib->dirExists(['foo', 'bar', '.txt']));
+        $this->assertFalse($lib->fileExists(['foo', 'bar', '.txt']));
 
         $this->assertTrue($lib->removeExtended(['foo', 'bar']));
-        $this->assertFalse($node->exists(['foo', 'bar', '.txt']));
+        $this->assertFalse($lib->exists(['foo', 'bar', '.txt']));
         $this->assertTrue($lib->removeDir(['foo', 'bar']));
-        $this->assertFalse($node->exists(['foo', 'bar']));
+        $this->assertFalse($lib->exists(['foo', 'bar']));
     }
 
     /**
