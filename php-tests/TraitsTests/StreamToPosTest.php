1c49fbe00c554431cbc3856658cea342c4bc344d
Petr Plsek <me@kalanys.com>
2023-06-17 00:20:08 +0200

    kw_files - tests, hints

--- php-tests/TraitsTests/StreamToPosTest.php
+++ php-tests/TraitsTests/StreamToPosTest.php
@@ -39,6 +39,8 @@ class StreamToPosTest extends \CommonTestClass
             ['abcdabcd', 'ABCD', null, 'ABCD'], // full replace
             ['abcdabcd', 'ABCD', 2, 'abABCDcd'], // replace inside
             ['abcd', 'ABCD', 6, "abcd\0\0ABCD"], // append at the end
+            ['abcdabcd', 'ABCDABCD', 0, 'ABCDABCD'], // overwrite
+            ['abcdabcd', 'ABCD', 6, 'abcdabABCD'], // replace overflow
             ['abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', null, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'], // full replace
             ['abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 13, 'abcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZnopqrstuvwxy'], // replace inside
             ['abcdefghijklmnopqrstuvwxy', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 30, "abcdefghijklmnopqrstuvwxy\0\0\0\0\0ABCDEFGHIJKLMNOPQRSTUVWXYZ"], // append at the end
